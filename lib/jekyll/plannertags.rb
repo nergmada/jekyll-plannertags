70c1889c0710c07a45d557375f1e55395eb677c6
Adam Green <adam.green@kcl.ac.uk>
2020-01-29 13:45:02 +0000

    Initialised

---
+++ lib/jekyll/plannertags.rb
@@ -0,0 +1,8 @@
+require "jekyll/plannertags/version"
+
+module Jekyll
+  module Plannertags
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/plannertags/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Plannertags
+    VERSION = "0.1.0"
+  end
+end
